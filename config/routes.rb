6b61a6c48631306c4a95c34acd39f6a4119245b0
藤枝　龍紀 <fujiedaryuuki@fujiedaryuukinoMacBook-Air.local>
2021-03-18 17:36:46 +0900

    [Add]課題4/Bookers2にいいね・コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'book_comments/create'
+  get 'book_comments/destroy'
+  get 'favorites/create'
+  get 'favorites/destroy'
   root to: 'home#top'
   get 'home/about'
   devise_for :users, controllers: {
@@ -6,6 +10,9 @@ Rails.application.routes.draw do
     registrations: 'devise/registrations'
   }
   resources :users, only:[:show, :index, :edit, :update]
-  resources :books, only:[:show, :index, :create, :edit, :update, :destroy]
+  resources :books, only: [:index, :show, :create, :edit, :update, :destroy] do
+    resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
